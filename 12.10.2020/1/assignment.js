8aa9d363e22cab01906d89f17b3fb4745a9b8140
DunkanProsk <deeeeeeeenchik@yandex.ru>
2020-10-12 22:35:28 +0300

    homework for ShellPea

---
+++ 12.10.2020/1/assignment.js
@@ -0,0 +1,17 @@
+let userNumber = prompt(''), result;
+
+result = 18 + (+userNumber);
+console.log('сумма = ' + result);
+
+result = 18 - userNumber;
+console.log('разность = ' + result);
+
+result = 18 * userNumber;
+console.log('умножение = ' + result);
+
+result = 18 / userNumber;
+console.log('деление = ' + result);
+
+alert('last result = ' + result + ' ; Число пользователя = ' + userNumber + ' ; Вычисления в консоле');
+alert('last result = ' + result + ' ; Число пользователя = ' + userNumber + ' ; Вычисления в консоле');
+
