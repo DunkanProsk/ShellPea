735d974b30bdb8738456f5a41185b741c9c5580c
DunkanProsk <deeeeeeeenchik@yandex.ru>
2020-11-19 15:55:37 +0300

    Homework from 19.11.2020

---
+++ 19.11.2020/5/assignment.js
@@ -0,0 +1,28 @@
+const sayHelloWithArguments = (say, name = 'Admin') => {
+  console.log(say + ' ' + name)
+}
+
+const sayHelloNoArguments = () => {
+  console.log('Hi Admin')
+}
+
+const saHelloReturn = () => {
+  return 'Hi Admin'
+}
+
+const checkInput = (callback, ...arg) => {
+	let i = 0
+
+	arg.map( function(arg) {
+		let value = (arg === '') ? i++ : false;
+	})
+
+	i === 1 ? callback() : callback()
+}
+
+checkInput(() => {console.log('Nice!')},'Hi','Hi','Hi','Hi','Hi','Hi','Hi','Hi')
+checkInput(() => {console.log('Empty line!')},'Hi','','Hi','Hi','Hi','Hi','Hi','Hi','Hi')
+
+sayHelloWithArguments('Hi', 'Elone')
+sayHelloWithArguments('Hi')
+sayHelloNoArguments()
